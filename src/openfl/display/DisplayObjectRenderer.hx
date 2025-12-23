0af9665d37cd2470a2f54e96f1104a9ed65158cf
swordcube <swordcube@noreply.localhost>
2025-12-22 19:02:08 -0600

    reapply pr #2797 but more throughly i think

--- src/openfl/display/DisplayObjectRenderer.hx
+++ src/openfl/display/DisplayObjectRenderer.hx
@@ -297,41 +297,57 @@ class DisplayObjectRenderer extends EventDispatcher
 			}
 			#end
 
-			if (hasFilters && !needRender)
+			if (hasFilters)
 			{
-				var affineChanged:Bool = updateTransform
-					&& __affineChanged(displayObject.__cacheBitmap.__worldTransform, displayObject.__worldTransform);
-
-				for (filter in displayObject.__filters)
-				{
-					if (filter.__renderDirty)
+			    inline function clearBitmapCache(displayObject:DisplayObject) {
+					// Make sure all of the bitmap data caches are disposed of
+					for (cache in [displayObject.__cacheBitmapData, displayObject.__cacheBitmapData2, displayObject.__cacheBitmapData3])
 					{
-						needRender = true;
-						break;
-					}
-					if (affineChanged && __isShaderFilter(filter))
-					{
-						// Make sure all of the bitmap data caches are disposed of
-						for (cache in [displayObject.__cacheBitmapData, displayObject.__cacheBitmapData2, displayObject.__cacheBitmapData3])
+						if (cache != null)
 						{
-							if (cache != null)
-							{
-								if (cache.__texture != null)
-									cache.__texture.dispose();
-								
-								cache.disposeImage();
-								cache.dispose();
-							}
-						}
-						// Then null them all out (including color transform cache) for the GC to pick up later
-						displayObject.__cacheBitmap = null;
-						displayObject.__cacheBitmapData = null;
-						displayObject.__cacheBitmapData2 = null;
-						displayObject.__cacheBitmapData3 = null;
-						displayObject.__cacheBitmapColorTransform = null;
+							if (cache.__texture != null)
+								cache.__texture.dispose();
 
-						needRender = true;
-						break;
+							cache.disposeImage();
+							cache.dispose();
+						}
+					}
+					// Then null them all out (including color transform cache) for the GC to pick up later
+					displayObject.__cacheBitmap = null;
+					displayObject.__cacheBitmapData = null;
+					displayObject.__cacheBitmapData2 = null;
+					displayObject.__cacheBitmapData3 = null;
+					displayObject.__cacheBitmapColorTransform = null;
+				}
+				if (!needRender)
+				{
+					var affineChanged:Bool = updateTransform
+						&& __affineChanged(displayObject.__cacheBitmap.__worldTransform, displayObject.__worldTransform);
+
+					for (filter in displayObject.__filters)
+					{
+						if (filter.__renderDirty)
+						{
+							needRender = true;
+							break;
+						}
+						if (affineChanged && __isShaderFilter(filter))
+						{
+						    clearBitmapCache(displayObject);
+							needRender = true;
+							break;
+						}
+					}
+				}
+				else
+				{
+					for (filter in displayObject.__filters)
+					{
+						if (__isShaderFilter(filter))
+						{
+						    clearBitmapCache(displayObject);
+							break;
+						}
 					}
 				}
 			}
